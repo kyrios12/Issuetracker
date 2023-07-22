a1c7562f5ce2e72897666d13bb9681dc89959a02
Abdul Azeez <103828615+kyrios12@users.noreply.github.com>
2023-07-23 00:20:37 +0530

    Creating Home Controller and Setting it in router

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
 const express = require('express');
 
 const router = express.Router();
+// Home controller to display home screen
+const homeController = require('../controllers/home_controller');
+router.get('/', homeController.home);
